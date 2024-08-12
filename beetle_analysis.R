aed9f52aecdc081de9eb139b8279d8be21375b65
Simon Gates <55692918+simonelgato@users.noreply.github.com>
2024-08-12 20:41:35 +0100

    12 August 2024
    wrote R prog for plotting data

---
+++ beetle_analysis.R
@@ -0,0 +1,98 @@
+## Gail's beetle data
+## 12 August 2024
+
+## script to do data reformatting and (hopefully) do some modelling
+
+setwd("C:/Users/notne/Research/GitHub/Gails_beetles")
+library(ggplot2)
+library(dplyr)
+
+################################################################################
+
+## read in data and make factors for field (field), location of sample (sloc: in crop 
+## or in margin) and sample number (snum)
+
+## I've calculated abundance and species richness already  in the input data set
+## did that in Excel
+
+beetle <- read.csv("beetles.csv")
+beetle$field <- factor(substr(beetle$Label, 1, 1))
+beetle$sloc <- factor(substr(beetle$Label, 4, 4))
+beetle$snum <- factor(substr(beetle$Label, 5, 5))
+beetle$year <- factor(substr(beetle$Season.and.Year, 8, 12))
+beetle$man <- factor(beetle$Management.Type)
+levels(beetle$man) <- c("con","int")
+
+################################################################################
+
+## plots
+
+## plot total abundance by field/year
+
+field_year <- beetle %>%
+  group_by(field, year) %>%
+  summarise(tot = sum(abundance),
+            sdtot = sd(abundance))
+
+ggplot(field_year, aes(x = year, y = tot)) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar(aes(ymin = tot-(2*sdtot), ymax = tot+(2*sdtot)), width = 0.5) +                                                                                            
+  facet_wrap(~field)
+  
+ggplot(field_year, aes(x = field, y = tot)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = tot-(2*sdtot), ymax = tot+(2*sdtot)), width = 0.5) +                                                                                            
+  facet_wrap(~year)
+
+################################################################################
+
+## as above but split additionally by management type
+
+trt <- beetle %>%
+  group_by(field, year, man) %>%
+  summarise(meanab = mean(abundance),
+            sdab = sd(abundance),
+            nobs = n())
+
+## grid of plots of conventional versus integrated for each field/year
+
+ggplot(trt, aes(x = man, y = meanab)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = meanab-(2*sdab), ymax = meanab+(2*sdab), width = 0.25)) +                                                                                            
+  facet_grid(field ~ year) +
+  xlab("Management type")
+  
+## plot as above but using all the raw data not averaging over all traps
+## and separating crop and margin sites by colour
+
+## CURRENTLY THIS PLOT HAS TOO MANY POINTS ON IT SO
+## SOMETHING IS GOING WRONG
+
+beetle %>%
+  group_by(field, year, man, sloc)
+
+ggplot(beetle, aes(x = man, y = abundance, colour = sloc)) +
+  geom_jitter(width = 0.15) +
+  facet_grid(field ~ year) +
+  xlab("Management type") +
+  scale_color_manual(values = c("C" = "steelblue", "M" = "tomato2"),
+                     labels = c("C" = "Crop", "M" = "Margin")) +
+  labs(color = "Location")
+  
+################################################################################
+
+## look at just 2016 data
+
+d2016 <- beetle %>%
+  filter(year == 2016)
+
+ggplot(d2016, aes(x = man, y = abundance, colour = sloc)) +
+#  geom_point() +
+## having geom_point and geom_jitter adds points twice
+  geom_jitter(width = 0.25) +
+  xlab("Management type") +
+  facet_grid(~ field) +
+  scale_color_manual(values = c("C" = "steelblue", "M" = "tomato2"),
+                     labels = c("C" = "Crop", "M" = "Margin")) +
+  labs(color = "Location")
